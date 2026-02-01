74ef9f0020c97e96a41950fdd886759d1d0ee153
Your Name Jairo-1995 <je.mamallactaa@uea.edu.ec>
2026-02-01 13:52:23 -0500

    adaptado archivo service y agregado comentarios

---
+++ Clone_repositorio/Mi_adaptacion_dashboard/servicios/archivo_serivice.py
@@ -0,0 +1,24 @@
+import os
+# Servicio para manejar operaciones de archivos
+# Creacion de la clase ArchivoService
+class ArchivoService:
+
+    @staticmethod
+    def listar_carpetas(ruta): # Listar carpetas en una ruta dada
+        return [f.name for f in os.scandir(ruta) if f.is_dir()]
+
+    @staticmethod
+    def listar_scripts(ruta):# Listar archivos .py en una ruta dada
+        return [f.name for f in os.scandir(ruta) if f.is_file() and f.name.endswith('.py')]
+
+    @staticmethod
+    def leer_codigo(ruta_script): # Leer el contenido de un archivo de script
+        try:
+            with open(ruta_script, 'r') as archivo: 
+                codigo = archivo.read()
+                print(f"\n--- Código de {ruta_script} ---\n")
+                print(codigo)
+                return codigo
+        except Exception as e:
+            print(f"Error: {e}")
+            return None
